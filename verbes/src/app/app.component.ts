8c04708a4ae69dfa94c04aaf86662fdf8a22b0e2
deniskarandashev <39792231+deniskarandashev@users.noreply.github.com>
2024-09-13 18:59:27 +0300

    build

--- verbes/src/app/app.component.ts
+++ verbes/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, ElementRef, inject, OnInit, QueryList, ViewChildren } from '@angular/core';
 import { DataService } from './data/data.service';
 import { Verb, VerbForms, VerbFormsType } from './data/data.model';
 
@@ -14,16 +14,26 @@ export class AppComponent implements OnInit {
 
   mode: string = 'questions';
 
+  @ViewChildren('inputElement') inputs!: QueryList<ElementRef>;
+
   ngOnInit(): void {
-      this.verbs = this.dataService.getData();
+    this.verbs = this.dataService.getData();
   }
 
-  onEnter($event: any, verb: Verb, prenom: string) {
+  onEnter($event: any, verb: Verb, prenom: string, i: number, j: number) {
     const el = document.getElementById(verb.verb + '_' + prenom)
     if (el) {
       if (verb.forms[prenom as VerbFormsType].includes($event?.target?.value)) {
         el.style.color = 'green';
         el.style.fontWeight = 'bold';
+
+        const currentIndex = i * this.prenoms.length + j;
+        const nextInput = this.inputs.get(currentIndex + 1);
+        if (nextInput) {
+          nextInput.nativeElement.focus();
+        }
+
+        $event.preventDefault();
       } else {
         el.style.color = 'red';
         el.style.fontWeight = 'bold';
